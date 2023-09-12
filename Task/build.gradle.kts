85f2ff2aa5c6d64595c5f334c690abe7b8225c47
woonseok105 <yws3786@gmail.com>
2023-09-12 17:23:23 +0900

    Add Spring Rest Docs & asciidocs

--- Task/build.gradle.kts
+++ Task/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
+    //ascii docs
+    id("org.asciidoctor.jvm.convert") version "4.0.0-alpha.1"
 }
 
 group = "com.DSM-Delivery-Backend"
@@ -37,6 +39,19 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 
+    // Spring Rest Docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    //asciidocs
+    implementation ("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    //kotest
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.4.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.4.0")
+    testImplementation("io.mockk:mockk:1.13.2")
+
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+
 //    implementation ("org.springframework.boot:spring-boot-starter-webflux")
 }
 
@@ -54,4 +69,41 @@ tasks.withType<Test> {
 
 kotlin.sourceSets.main {
     kotlin.srcDir("$buildDir/generated/source/kapt/main")
+}
+
+// Ascii Doc Snippet Directory
+val snippetsDir by extra { file("build/generated-snippets") }
+
+// Ascii Doc Create Tasks
+tasks {
+    // Test 결과를 snippet Directory에 출력
+    test {
+        outputs.dir(snippetsDir)
+    }
+
+    asciidoctor {
+        // test 가 성공해야만, 아래 Task 실행
+        dependsOn(test)
+
+        // 기존에 존재하는 Docs 삭제(문서 최신화를 위해)
+        doFirst {
+            delete(file("src/main/resources/static/docs"))
+        }
+
+        // snippet Directory 설정
+        inputs.dir(snippetsDir)
+
+        // Ascii Doc 파일 생성
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("src/main/resources/static/docs")
+            }
+        }
+    }
+
+    build {
+        // Ascii Doc 파일 생성이 성공해야만, Build 진행
+        dependsOn(asciidoctor)
+    }
 }
